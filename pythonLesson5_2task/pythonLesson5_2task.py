bb83f756771cf6c2ef4fe3c7bc259b881028fb2a
MaksimZ91 <ak4202@mail.ru>
2022-12-08 15:37:46 +0800

    task 2 in progress

--- pythonLesson5_2task/pythonLesson5_2task.py
+++ pythonLesson5_2task/pythonLesson5_2task.py
@@ -8,15 +8,15 @@
 
 import random
 
-candies = 2021
 
-player_1_papth = "fisrt_player_canides.txt"
-player_2_papth = "second_player_candies_.txt"
-candies_player_1 = 0
-candies_player_2 = 0
+pathCandies = "./pythonLesson5_2task/candies.txt"
+pathPlayer1 = "./pythonLesson5_2task/fisrt_player_canides.txt"
+pathPlayer2 = "./pythonLesson5_2task/second_player_candies.txt"
 
-def step(path, candies):
-    with open(path, "r+") as read:
+
+
+def step(candies, pathCandies, pathPlayer):
+    with open(pathPlayer, "r+") as read:
         player_candis = int(read.read())
     limit = 28
     if candies < limit:
@@ -24,16 +24,17 @@ def step(path, candies):
     try:
         candie = int(input("Какое колличество конфеты вы хотите забрать: "))
         while 1 > candie or candie > limit:
-            print(f"Общие колличество конфет уменьшилось до {candies}") if candie > limit else "" 
+            print(f"Общие колличество конфет уменьшилось до {candies}") if candie > limit else "" # доработать
             candie = int(input("Какое колличество конфеты вы хотите забрать: "))
     except:
         print(f"Значение дожлны быть число от 1 до {limit}.")
         candie = int(input("Какое колличество конфеты вы хотите забрать: "))     
-    with open(path, "w") as data:          
+    with open(pathPlayer, "w") as data:          
         player_candis += candie
         candies -= candie
-        data.write(str(player_candis))  
-           
+        data.write(str(player_candis))
+    with open(pathCandies, "w") as data:
+        data.write(str(candies)) 
     print(f"Количество конфет у игорка № 1: {player_candis}, всего конфет осталось {candies}" )     
     return candie
 
@@ -52,25 +53,42 @@ def lottery():
 
 
 
-def game():
-    candies = 100       
-    player_1_path = "./pythonLesson5_2task/fisrt_player_canides.txt"
-    player_2_path = "./pythonLesson5_2task/second_player_candies.txt"
+def game( pathCandies, pathPlayer1, pathPlayer2 ):
+    init_game( pathCandies, pathPlayer1, pathPlayer2 )
+    with open(pathCandies, "r+") as read:
+         candies = int(read.read())          
+
     players = lottery()
     while candies > 0:     #вынести функционал под if в отдельну функцию или добавить в step
+        with open(pathCandies, "r+") as read:
+            candies = int(read.read())
         if players["Игрок 1"]:
             print("Ход игорка №1")
-            st = step(player_1_path, candies)
-            candies -= st            
+            st = step(candies, pathCandies, pathPlayer1 )
+                     
             players["Игрок 1"] = False            
             
         else: 
             print("Ход игорка №2")
-            st = step(player_2_path, candies)
-            candies -= st            
+            st = step(candies,pathCandies, pathPlayer2)                    
             players["Игрок 1"] = True 
     print(candies)   
 
 
 
-game()
+
+
+def init_game(pathCandies, pathPlayer1, pathPlayer2):
+    candies = 100
+    firstPlayerCandies = 0   
+    secondPlayerCandies = 0     
+    with open(pathCandies, "w") as gameCandies:
+        gameCandies.write(str(candies))
+    with open(pathPlayer1, "w") as player1Candies:
+        player1Candies.write(str(secondPlayerCandies))
+    with open(pathPlayer2, "w") as player2Candies:
+        player2Candies.write(str(firstPlayerCandies))        
+
+
+
+game(pathCandies, pathPlayer1, pathPlayer2)
