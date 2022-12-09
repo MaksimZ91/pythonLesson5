f29420fcc834a28696d279a68ebc890ba066e836
MaksimZ91 <ak4202@mail.ru>
2022-12-09 14:53:58 +0800

    task 3 i progress

--- pythonLesson5_3task/pythonLesson5_3task.py
+++ pythonLesson5_3task/pythonLesson5_3task.py
@@ -1,7 +1,6 @@
 #Создайте программу для игры в ""Крестики-нолики"".
-#Реализовать смену игроков / символов
 #Реализовать проверка победы
-#Доделать валидацию
+
 
 def printBoard(board):    
     for i in board:                 
@@ -14,11 +13,11 @@ def printBoard(board):
 def userStep(board, symbol):
     try:
         userCords = list(map(int,input("Введите координаты через пробел в формате строка - стобец: ").split()))
-        while (userCords[0] <= 0 or userCords[0] >= 4) or (userCords[1] <= 0 or userCords[1] >= 4):
-          userCords = list(map(int,input("Введите координаты через пробел в формате строка - стобец: ").split()))
     except:
-        print("Введены не коректные данные")    
-    
+        print("Введены не коректные данные")
+        userCords = list(map(int,input("Введите координаты через пробел в формате строка - стобец: ").split()))
+    while (userCords[0] <= 0 or userCords[0] >= 4) or (userCords[1] <= 0 or userCords[1] >= 4):
+        userCords = list(map(int,input("Введите координаты через пробел в формате строка - стобец: ").split()))
     while not board[userCords[0]-1][userCords[1]-1] == ("*" or symbol):
         print("Ячейка занята введите другую координату!!!")
         userCords = list(map(int,input("Введите координаты через пробел в формате строка - стобец: ").split()))
@@ -26,44 +25,59 @@ def userStep(board, symbol):
     return board
 
 def winerChek(board, winerBoard):
+    stringBoard = []
+    for i in board:
+        for j in i:
+            stringBoard.append(j)
+    tmp = ""        
+    tmp = tmp.join(stringBoard)     
+    for k in winerBoard:       
+        print(k, tmp)
+      
+   
+   
+ 
+        
+
+      
+
+
     #Выдает True 
     #Три услвия выдачи False 1.Победа Х 2.Победа О 3.Ничья
 
 
     return True
 
-winerBoard = [[("X", "X", "X"),(0, 0, 0),(0, 0, 0)],
-              [(0, 0, 0),("X", "X", "X"), (0, 0, 0)],
-              [(0, 0, 0),(0, 0, 0),("X", "X", "X")],
-              [("X", 0, 0),("X", 0, 0),("X", 0, 0)],
-              [(0, "X", 0),(0, "X", 0), (0, "X", 0)],
-              [(0, 0, "X"),(0, 0, "X"),(0, 0, "X")],
-              [("X", 0, 0),(0, "X", 0), (0, 0, "X")],
-              [(0, 0, "X"),(0, "X", 0),("X", 0, 0)],
-               ]
+winerBoard = ["XXX******", "***XXX***", "******XXX", 
+              "X**X**X**", "*X**X**X*", "**X**X**X",
+              "X***X***X", "**X*X*X**"]
 
 
 
 
 
 
-def game():
+def game(winerBoard):
     print("Игра началась.")
     board = [["*"]*3 for i in range(3)]
     printBoard(board)
     flag = True
-    chek = winerChek(board, winerBoard)
+    chek = True
     while chek:
         if flag:
+            print("Ходят 'X' ")
             symbol= "X"
             board = userStep(board, symbol)
+            chek = winerChek(board, winerBoard)
             printBoard(board)
             flag = False
         else:
+            print("Ходят 'O' ")
             symbol= "O"
             board = userStep(board, symbol)
+            chek = winerChek(board, winerBoard)
             printBoard(board)
             flag = True
 
 
-game()
+game(winerBoard)
