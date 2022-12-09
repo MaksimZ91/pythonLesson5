ab072916fb1b28e526297107ae2b23307db3bf27
MaksimZ91 <ak4202@mail.ru>
2022-12-09 18:36:39 +0800

    task 3 complited

--- pythonLesson5_3task/pythonLesson5_3task.py
+++ pythonLesson5_3task/pythonLesson5_3task.py
@@ -1,99 +1,61 @@
 #Создайте программу для игры в ""Крестики-нолики"".
-#Реализовать проверка победы
-
 
 def printBoard(board):    
-    for i in board:                 
+    for i in range(3):                
         print("-------------")
-        print(f"| {i[0]} | {i[1]} | {i[2]} |")
-        
-
-
+        print(f"| {board[0+i*3]} | {board[1+i*3]} | {board[2+i*3]} |")
 
 def userStep(board, symbol):
     try:
-        userCords = list(map(int,input("Введите координаты через пробел в формате строка - стобец: ").split()))
+        userCords = int(input(f"Введите число вместо которго хотит поставить {symbol}: "))
+        while (userCords < 1 or userCords > 9):
+            userCords = int(input(f"Введите число вместо которго хотит поставить {symbol}: "))
+        while board[userCords-1] == ("X" or "O"):
+            print("Ячейка занята введите другое число!!!")
+            userCords = int(input(f"Введите число вместо которго хотит поставить {symbol}: "))
     except:
         print("Введены не коректные данные")
-        userCords = list(map(int,input("Введите координаты через пробел в формате строка - стобец: ").split()))
-    while (userCords[0] <= 0 or userCords[0] >= 4) or (userCords[1] <= 0 or userCords[1] >= 4):
-        userCords = list(map(int,input("Введите координаты через пробел в формате строка - стобец: ").split()))
-    while not board[userCords[0]-1][userCords[1]-1] == ("Z" or symbol):
-        print("Ячейка занята введите другую координату!!!")
-        userCords = list(map(int,input("Введите координаты через пробел в формате строка - стобец: ").split()))
-    board[userCords[0]-1][userCords[1]-1] = symbol
+        userCords = int(input(f"Введите число вместо которго хотит поставить {symbol}: "))      
+    board[userCords-1] = symbol
     return board
 
-def winerChek(board, winerBoard):
-    stringBoard = []
-    for i in board:
-        for j in i:
-            stringBoard.append(j)
-    tmp = ""        
-    tmp = tmp.join(stringBoard)     
-    for k in winerBoard:       
-        if tmp[0] == k[0] and tmp[1] == k[1] and tmp[2] == k[2]:
-            return False
-        elif tmp[3] == k[3] and tmp[4] == k[4] and tmp[5] == k[5]:
-            return False
-        elif tmp[6] == k[6] and tmp[7] == k[7] and tmp[8] == k[8]:
-            return False  
-        elif tmp[0] == k[0] and tmp[3] == k[3] and tmp[5] == k[5]:
-            return False
-        elif tmp[1] == k[1] and tmp[4] == k[4] and tmp[7] == k[7]:
-            return False
-        elif tmp[2] == k[2] and tmp[5] == k[5] and tmp[8] == k[8]:
-            return False  
-        elif tmp[0] == k[0] and tmp[4] == k[4] and tmp[8] == k[8]:
-            return False  
-        elif tmp[2] == k[2] and tmp[4] == k[4] and tmp[6] == k[6]:
-            return False                   
-
-      
-   
-   
- 
-        
-
-      
-
-
-    #Выдает True 
-    #Три услвия выдачи False 1.Победа Х 2.Победа О 3.Ничья
-
-
+def winerChek(board):
+    winerBoard = ((0,1,2), (3,4,5), (6,7,8), (0,3,6), (1,4,7), (2,5,8), (0,4,8), (2,4,6))
+    for i in winerBoard:        
+        if board[i[0]] == board[i[1]] == board[i[2]]:
+           print(f"Игра окончена победил {board[i[0]]}!!!")
+           return False
     return True
 
-winerBoard = ["XXX******", "***XXX***", "******XXX", 
-              "X**X**X**", "*X**X**X*", "**X**X**X",
-              "X***X***X", "**X*X*X**"]
-
-
-
-
-
-
-def game(winerBoard):
+def game():
+    count = 0
     print("Игра началась.")
-    board = [["Z"]*3 for i in range(3)]
+    board = [ i for i in range(1,10)]    
     printBoard(board)
     flag = True
     chek = True
     while chek:
-        if flag:
+        print(count)
+        if flag:           
             print("Ходят 'X' ")
             symbol= "X"
-            board = userStep(board, symbol)
-            chek = winerChek(board, winerBoard)
+            board = userStep(board, symbol)            
             printBoard(board)
             flag = False
+            count+=1
         else:
             print("Ходят 'O' ")
             symbol= "O"
-            board = userStep(board, symbol)
-            chek = winerChek(board, winerBoard)
+            board = userStep(board, symbol)            
             printBoard(board)
             flag = True
+            count+=1
+        if count > 4:
+            chek = winerChek(board)
+            if count == 9:
+                print("Ничья!")
+                chek = False        
 
 
-game(winerBoard)
+                
+game()
